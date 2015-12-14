d475ed0cc0fddb8f1c528c77380b457f7ebf0e7a
nandaverdeny <verdenynanda@gmail.com>
2015-12-14 13:33:28 +0700

    Initial Create

---
+++ Sperience/Sperience/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Sperience;
+using SperienceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SperienceWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        SperienceEntities db = new SperienceEntities();
+        HomeModel viewModel = new HomeModel();
+        
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<HomeModel> result = new List<HomeModel>();
+            IEnumerable<Stage> queryStages = db.Database.SqlQuery<Stage>("SELECT * FROM crm.Stage");
+            List<Stage> stageResult = queryStages.ToList();
+            foreach (var stage in stageResult)
+            {
+                IEnumerable<SubjectHeader> subjectResult = db.Database.SqlQuery<SubjectHeader>("SELECT * FROM crm.SubjectHeader WHERE StageId = " + stage.Id + "");
+                result.Add(new HomeModel() { StagesModel = stage, SubjectsModel = subjectResult.ToList() });
+            }
+            ViewBag.result = result.ToList();
+            return View();
+        }
+    }
+}
---
+++ Sperience/Sperience/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using Sperience;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SperienceWeb.Models
+{
+    public class HomeModel
+    {
+        public Stage StagesModel { get; set; }
+        public List<SubjectHeader> SubjectsModel { get; set; }
+    }
+}
